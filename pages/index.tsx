370757fd5307e2012d7466db11284d5623bf95af
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-04-04 10:49:40 -0300

    feat: get data from static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
 import Head from 'next/head';
 import Link from 'next/link';
+
+import { getSortedPostsData } from '../lib/posts';
+
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -18,6 +30,20 @@ export default function Home() {
         <div>
           <Link href="/posts/first-post">Go to Posts</Link>
         </div>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
